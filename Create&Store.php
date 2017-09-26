de0453142712d2fde4b1cf0ce675f4a61717ec90
NischalB744 <basnetn744@gmail.com>
2017-09-26 14:12:57 -0500

    Create QR Codes using PHP and store it on the database.

--- Create&Store.php
+++ Create&Store.php
@@ -4,75 +4,168 @@
     <head>
         <title> Create and Store the QR Code </title>
         
-        <script type = "text/javascript" src = "jquery.js"></script>
-        <script type = "text/javascript">
-             function submitForm()
-                {
-                    
-                    var itemValue = document.getElementById("itemNumber").value;
-                    var lotValue = document.getElementById("lotNumber").value;
-                    var expValue = document.getElementById("expiryDate").value;
-                    var batchValue = document.getElementById("batchNumber").value;
-                    
-                    var finalString = itemValue + "," +lotValue + "," + expValue + "," + batchValue;
-                    
-                    
-                }
-
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="pageStyle.css">
+        
+        
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+        
+        <script type = "text/javascript" src = "js/jquery.js"></script>
+        
+        <script type="text/javascript">
+        
+        
+            function printLabelSuccess()
+            {
+                document.getElementById("databaseLabel").innerHTML = "";
+                
+                
+                var successDiv = document.createElement("div");
+                var successMsg = document.createTextNode("The item was successfully stored in the database.");
+                successDiv.appendChild(successMsg);
+                successDiv.setAttribute("class","alert alert-success");
+                successDiv.setAttribute("role","alert");
+                document.getElementById("feedbackContainer").appendChild(successDiv);
+            }
+            
+            
+            function printLabelFail()
+            {
+                
+                document.getElementById("databaseLabel").innerHTML = "";
+                
+                var successDiv = document.createElement("div");
+                var successMsg = document.createTextNode("Error entering the item in the database. Could be duplicate entry or an invalid entry. Please try again or check the database.");
+                successDiv.appendChild(successMsg);
+                successDiv.setAttribute("class","alert alert-danger");
+                successDiv.setAttribute("role","alert");
+                
+                document.getElementById("feedbackContainer").appendChild(successDiv);
+               
+            }
+        
         </script>
-
+        
+        
     </head>
+    
+    
 
     <body>
-        <form action = "" method="post">
-         <div id = "item_num">
-                <label for  = "itemNumber">ITEM NUMBER: </label>
-                <input type = "text" id = "itemNumber" name  = "itemNumber">
-            </div>
+        
+        <nav class="navbar navbar-expand-lg navbar-light myNavBar example-screen" style = "background:#539AC6;">
+          <a class="navbar-brand text-white" href="#"><img src = "image/logo.png"></a>
+
+                <h1 id = "pageHeading" class = "display-3 col col-sm-12">AOSS QR Storage System</h1>
+          
+        </nav>
+        
+        
+            <form action = "" method="post">
+             <div id = "item_num">
+                    <label for  = "itemNumber">ITEM NUMBER: </label>
+                    <input type = "text" id = "itemNumber" name  = "itemNumber" required = "required">
+                </div>
+
+                <div id = "lot_num">
+                    <label for  = "lotNumber">LOT NUMBER: </label>
+                    <input type = "text" id = "lotNumber" name = "lotNumber" required = "required">
+                </div>
+
+                <div id = "exp_date">
+                    <label for  = "expiryDate">EXPIRATION DATE: </label>
+                    <input type = "text" id = "expiryDate" name = "expiryDate" required = "required">
+                </div>
+
+                <div id = "batch_num">
+                    <label for  = "batchNumber">BATCH NUMBER: </label>
+                    <input type = "text" id = "batchNumber" name = "batchNumber" >
+                </div>
+
+                <div id = "submit_btn">
+                    <input type="submit" value="CREATE &amp; STORE">
+                </div>
+            </form>
+        
+        
+        <div id = "infoLabel">
             
-            <div id = "lot_num">
-                <label for  = "lotNumber">LOT NUMBER: </label>
-                <input type = "text" id = "lotNumber" name = "lotNumber">
+        </div>
+        
+        <div id = "feedbackContainer">
+            <div id ="databaseLabel">
+                
             </div>
-            
-            <div id = "exp_date">
-                <label for  = "expiryDate">EXPIRATION DATE: </label>
-                <input type = "text" id = "expiryDate" name = "expiryDate">
-            </div>
-            
-            <div id = "batch_num">
-                <label for  = "batchNumber">BATCH NUMBER: </label>
-                <input type = "text" id = "batchNumber" name = "batchNumber" >
-            </div>
-            
-            <div id = "submit_btn">
-                <button type="submit" onclick = "submitForm()">CREATE &amp; STORE</button>
-            </div>
-        </form>
+        </div>
+        
         <?php
             
+            error_reporting(0);
             
             $item = $_POST['itemNumber'];
             $lot = $_POST['lotNumber'];
             $exp = $_POST['expiryDate'];
             $batch = $_POST['batchNumber'];
-            
-            
-            
-            
-            echo '<img src="qrCode.php?item1='.$item.'&lot1='.$lot.'&exp1='.$exp.'&batch1='.$batch.'"/>';
-            
+            $barcode = $item.",".$lot.",".$exp.",".$batch;
+        
+            if(($item == '')||($lot == '')||($exp == ''))
+            {
+                // Do Nothing (Is taken care by the HTML Itself)
+            }
+            else
+            {
+                 $mysqli = new mysqli('127.0.0.1','root','','test');
+            if ($mysqli->connect_error){
+                die('Could not connect to database!');
+            }
+
+
+
+            $statement = $mysqli->prepare("INSERT INTO barcodetest VALUES (?, ?, ?, ?, ?)");
+
+            $statement->bind_param('sssss',$barcode,$item,$lot,$exp,$batch);
+
+            $success = $statement->execute();
+
+
+
+            if($success) 
+            {
+
+                echo '<script>';
+                echo 'printLabelSuccess()';
+                echo '</script>';
+                echo '<img width="150" height = "150" src="qrCode.php?item1='.$item.'&lot1='.$lot.'&exp1='.$exp.'&batch1='.$batch.'"/>'; 
+                echo "</br>";
+                echo "</br>";
+                echo "Item #: ".$item."</br>";
+                echo "Lot #: ".$lot."</br>";
+                echo "Expiry Date: ".$exp."</br>";
+
+                if(!($batch == ''))
+                {
+                    echo "Batch #: ".$batch."</br>";
+                }
+            }
+            else
+            {
+                echo '<script>';
+                echo 'printLabelFail()';
+                echo '</script>';
+            }
+                
+            }
+        
+          
+        
+        
+        
         ?>
         
-        <p>Item Number: </p>
-        <p>Lot Number: </p>
-        <p>Expiry Date: </p>
-        <p>Batch Number:</p>
         
-        
-        
-        
-            
         
         
 
--- qrcode.php
+++ qrcode.php
@@ -5,22 +5,7 @@
                 
                 $qrData = $_GET['item1'].",".$_GET['lot1'].",".$_GET['exp1'].",".$_GET['batch1'];
                 QRcode::png($qrData);    
-                      
-            
-/*
-            $qr = new QrCode('this is awesome');
 
-            $qr->setSize(100);
-
-            $image = $qr->writeString();
-
-
-
-            //header('Content-Type: '.$qr->getContentType());
-
-            echo $image;
-
-*/
 
         ?>
         
